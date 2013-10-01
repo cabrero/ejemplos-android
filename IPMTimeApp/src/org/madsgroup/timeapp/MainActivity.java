19540ebb0195a8c3362ef1985c23dab8ba484b56
David Cabrero <david.cabrero@madsgroup.org>
2013-10-02 01:36:41 +0200

    === Hello Time === Versión del hola mundo que dice la hora.

--- IPMTimeApp/src/org/madsgroup/timeapp/MainActivity.java
+++ IPMTimeApp/src/org/madsgroup/timeapp/MainActivity.java
@@ -2,6 +2,8 @@ package org.madsgroup.timeapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +13,11 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	Time now = new Time();
+	now.setToNow();
+	String labelText = String.format(getString(R.string.isTime),
+					 now.format("%H:%M"));
+	TextView tv = (TextView) findViewById(R.id.label4time);
+	tv.setText(labelText);
     }
 }
