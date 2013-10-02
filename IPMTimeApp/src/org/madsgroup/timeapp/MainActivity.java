b4a484a0a0d9be47791b5e6c1baa74f3ffa5176b
David Cabrero <david.cabrero@madsgroup.org>
2013-10-02 18:09:49 +0200

    Añadido un "timer" para que actualize la hora al pasar un minuto, mientras la app está activa.

--- IPMTimeApp/src/org/madsgroup/timeapp/MainActivity.java
+++ IPMTimeApp/src/org/madsgroup/timeapp/MainActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.widget.TextView;
+import android.os.Handler;
 
 public class MainActivity extends Activity
 {
 	
     private TextView _tvTime;
+    private Handler _handler;
 
     /** Called when the activity is first created. */
     @Override
@@ -18,16 +20,29 @@ public class MainActivity extends Activity
         setContentView(R.layout.main);
 
 	_tvTime = (TextView) findViewById(R.id.label4time);
+	_handler = new Handler();
     }
 
     @Override
     public void onResume() 
     {
 	super.onResume();
+        _updateTime();
+    }
+
+    private void _updateTime()
+    {
 	Time now = new Time();
 	now.setToNow();
 	String labelText = String.format(getString(R.string.isTime),
 					 now.format("%H:%M"));
 	_tvTime.setText(labelText);
+	long delay = Math.abs(60 - now.second) * 1000;
+	_handler.postDelayed(new Runnable() {
+			public void run() {
+				MainActivity.this._updateTime();
+			}
+		},
+		delay);
     }
 }
